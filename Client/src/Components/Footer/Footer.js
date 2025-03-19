f84821be9af4a34c68d373e968455899777fc19c
SandhyaChauhan18 <sandhyachauhan592@gmail.com>
2025-03-19 19:40:07 +0530

    first-commit

---
+++ Client/src/Components/Footer/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "./images/logo.png";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div>
+        <Link className="logo-container" to="/">
+          <img className="navbar-logo" src={logo} alt="PawFinds Logo" />
+          <p>{props.title}</p>
+        </Link>
+      </div>
+      <div className="below-footer">
+        <p>
+          You can reach us at{" "}
+          <a className="mail-links" href="mailto:techrangersofficial@gmail.com">
+          techrangersofficial@gmail.com
+          </a>
+        </p>
+        <p>
+          <a
+            className="contact-links"
+            href="https://www.linkedin.com/in/techrangersofficial/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-linkedin-square"></i> Linkedin
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://github.com/techrangersofficial"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-github"></i> GitHub
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://www.instagram.com/techrangersofficial/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-instagram"></i> Instagram
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://api.whatsapp.com/qr/GXRHM7PEPNOKA2?autoload=1&app_absent=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-whatsapp"></i> WhatsApp
+          </a>
+        </p>
+        <p>&copy; 2025 Tech Rangers</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
